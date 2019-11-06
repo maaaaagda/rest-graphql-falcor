ca4034921d39d6c06c1161069a55cd29276f752b
Maciej Kozik <m.kozik@iambot.ai>
2019-11-06 10:55:16 +0100

    add redux, fixes

--- frontend/src/Root.tsx
+++ frontend/src/Root.tsx
@@ -1,13 +1,19 @@
 // App.js - react-hot-loader >= 4.5.4
 import React from 'react'
+import { Provider } from 'react-redux'
 import { RestfulProvider } from 'restful-react'
 import App from './App'
 import { hot } from 'react-hot-loader/root'
+import { configureStore } from './store'
+
+const store = configureStore()
 
 const Root = () => (
-  <RestfulProvider base={process.env.REACT_APP_API_ENDPOINT as string}>
-    <App />
-  </RestfulProvider>
+  <Provider store={store}>
+    <RestfulProvider base={process.env.REACT_APP_API_ENDPOINT as string}>
+      <App />
+    </RestfulProvider>
+  </Provider>
 )
 
 export default process.env.NODE_ENV === 'development' ? hot(Root) : Root
