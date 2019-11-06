8fb5d415f6851b52e03de1ffc7e13d7c75d3a87f
kozikkam <kamil.kozik@hotailors.com>
2019-11-06 12:12:00 +0100

    Add v1 of navbar

--- frontend/src/common/styles/_constants.ts
+++ frontend/src/common/styles/_constants.ts
@@ -1,5 +1,6 @@
 export enum PALETTE {
-  Primary = '409e18',
-  Secondary = 'DBD5B5',
-  Background = 'ffffff',
+  Primary = '#409e18',
+  Secondary = '#DBD5B5',
+  Background = '#ffffff',
+  CallToAction = '#e60000',
 }
--- frontend/src/components/Navbar/Navbar.tsx
+++ frontend/src/components/Navbar/Navbar.tsx
@@ -1,27 +1,35 @@
-import React from 'react'
 import { Navbar } from '@blueprintjs/core'
+import React from 'react'
+import { PALETTE } from '../../common/styles/_constants'
 import styles from './styles.module.scss'
 
 type Props = {}
 
 export const MainNavbar = ({  }: Props) => (
-  <header className="App-header">
-    <Navbar className="bp3-navbar bp3-dark" fixedToTop>
-      <div className="navbar-content">
-        <div className="bp3-navbar-group bp3-align-left">
-          <div className="bp3-navbar-heading">Blueprint</div>
-        </div>
-        <div className="bp3-navbar-group bp3-align-right">
-          <button className="bp3-button bp3-minimal bp3-icon-home">Home</button>
-          <button className="bp3-button bp3-minimal bp3-icon-document">
-            Files
-          </button>
-          <span className="bp3-navbar-divider"></span>
-          <button className="bp3-button bp3-minimal bp3-icon-user"></button>
-          <button className="bp3-button bp3-minimal bp3-icon-notifications"></button>
-          <button className="bp3-button bp3-minimal bp3-icon-cog"></button>
-        </div>
+  <Navbar
+    className={styles.appHeader}
+    style={{ backgroundColor: PALETTE.Background }}
+    fixedToTop>
+    <div className={styles.navbarContent}>
+      <div className="bp3-navbar-group bp3-align-left">
+        <img className={styles.navbarLogo} src="logo200.png"></img>
       </div>
-    </Navbar>
-  </header>
+      <div className="bp3-navbar-group bp3-align-right">
+        <button className={styles.headerButton}>DIETY</button>
+        <span className="bp3-navbar-divider"></span>
+        <button className={styles.headerButton}>MENU</button>
+        <span className="bp3-navbar-divider"></span>
+        <button className={styles.headerButton}>GDZIE DOWOZIMY</button>
+        <span className="bp3-navbar-divider"></span>
+        <button
+          className={styles.headerCTAButton}
+          style={{
+            backgroundColor: PALETTE.CallToAction,
+            color: PALETTE.Background,
+          }}>
+          ZAMÓW
+        </button>
+      </div>
+    </div>
+  </Navbar>
 )
