e2bd4ef8f4c342cd3ca290c5245a8794291f6efc
magda <magdapolak96@gmail.com>
2019-11-09 20:07:56 +0100

    Auth endpoints with new structure and soe user endpoints fixes

--- backend/src/api/auth/ioc/inversify.config.ts
+++ backend/src/api/auth/ioc/inversify.config.ts
@@ -18,6 +18,7 @@ import { UserRepository } from "../../user/repository/UserRepository";
 import { ILoginController } from "../controller/loginController/ILoginController";
 import { LoginController } from "../controller/loginController/LoginController";
 import { AUTH_TYPES } from "./AuthTypes";
+import { AuthService } from "../service/AuthService";
 
 const getContainer: () => Container = (): Container => {
   const container: Container = new Container();
@@ -52,6 +53,10 @@ const getContainer: () => Container = (): Container => {
 
   container.bind<IAuthenticator>(TYPES.IAuthenticator)
     .to(Authenticator)
+    .inSingletonScope();
+
+  container.bind<AuthService>(AUTH_TYPES.IAuthService)
+    .to(AuthService)
     .inSingletonScope();
 
   return container;
