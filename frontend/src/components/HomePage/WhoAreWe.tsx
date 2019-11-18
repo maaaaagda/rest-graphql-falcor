6f72d6ad1f1712305093433bdf3e542aaf2d09c4
magda <magdapolak96@gmail.com>
2019-11-18 21:55:58 +0100

    Who are we basic section implemented

--- frontend/src/components/HomePage/WhoAreWe.tsx
+++ frontend/src/components/HomePage/WhoAreWe.tsx
@@ -1,11 +1,22 @@
 import React from 'react'
 import classNames from 'classnames'
+import s from './styles.module.scss'
 
 const WhoAreWe: React.FC = props => {
   return (
     <div className={'row'}>
-      <div className={classNames('col-md-7', 'col-sm-12')}>h65h</div>
-      <div className={classNames('col-md-5', 'col-sm-12')}>h6h5</div>
+      <div className={classNames('col-lg-7', 'col-sm-12')}>
+        <img className={s.mealImage} src={'home_meal_1.jpg'} alt={''} />
+      </div>
+      <div className={classNames('col-lg-5', 'col-sm-12')}>
+        <div>
+          <h1>Kim jesteśmy?</h1>
+          <p>
+            W Diet In A Box zajmujemy się przyrządzaniem pysznych i zdrowych
+            posiłków oraz dostarczaniem ich do Twojego domu
+          </p>
+        </div>
+      </div>
     </div>
   )
 }
