d304cca101a8b1b60b208e20bb11be399f3ea634
magda <magdapolak96@gmail.com>
2019-11-19 22:28:18 +0100

    Really eko section created

--- frontend/src/components/HomePage/HomePage.tsx
+++ frontend/src/components/HomePage/HomePage.tsx
@@ -2,12 +2,14 @@ import React from 'react'
 import { Card } from '@blueprintjs/core'
 import WhoAreWe from './WhoAreWe'
 import WhyWe from './WhyWe'
+import ReallyEko from './ReallyEko'
 
 const HomePage: React.FC = props => {
   return (
     <Card className="px-5">
       <WhoAreWe />
       <WhyWe />
+      <ReallyEko />
     </Card>
   )
 }
