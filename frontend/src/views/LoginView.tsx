17fe90587d351c0951eef6c3632367ae95a9d2e9
Maciej Kozik <m.kozik@iambot.ai>
2019-11-20 12:34:04 +0100

    add logo to login page

--- frontend/src/views/LoginView.tsx
+++ frontend/src/views/LoginView.tsx
@@ -1,10 +1,22 @@
 import React from 'react'
 import { LoginForm } from 'src/components/LoginForm'
+import { Container, Row, Col } from 'react-bootstrap'
+import logo from 'src/media/logo200.png'
+import styles from './LoginView.module.scss'
 
 const LoginView = () => (
-  <React.Fragment>
-    <LoginForm />
-  </React.Fragment>
+  <Container fluid={true}>
+    <div className={styles.logo}>
+      <img src={logo} className={styles.logoImage} alt="logo" />
+    </div>
+    <Row>
+      <Col md="3" className="px-0" />
+      <Col md="6" className="px-0">
+        <LoginForm />
+      </Col>
+      <Col md="3" className="px-0" />
+    </Row>
+  </Container>
 )
 
 export { LoginView }
