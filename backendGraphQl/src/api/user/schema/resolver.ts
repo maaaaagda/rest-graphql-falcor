fc703155106fc220fce4889feed5cbd02433a6d9
magdalena <magdapolak@gmail.com>
2020-02-22 21:35:41 +0100

    feat: inversify with graphql now getting along

--- backendGraphQl/src/api/user/schema/resolver.ts
+++ backendGraphQl/src/api/user/schema/resolver.ts
@@ -1,33 +1,28 @@
-import { injectable, inject } from "inversify";
+import { injectable, inject, Container } from "inversify";
 import { IAuthenticator } from "../../../core/auth/IAuthenticator";
 import { TYPES } from "../../../ioc/types";
 import { IUser } from "../model/User";
 import { IUserService } from "../service/IUserService";
 import { USER_TYPES } from "../ioc/UserTypes";
 import { IGetUserController } from '../controller/getUserController/IGetController';
+import getContainer from "../ioc/inversify.config";
 
 @injectable()
-export class GetUserController implements IGetUserController {
+export class UserResolver implements IGetUserController {
+
+    private readonly _container: Container = getContainer();
 
     @inject(USER_TYPES.IUserService)
-    private readonly _userService: IUserService;
+    private readonly _userService: IUserService = this._container.get(
+        USER_TYPES.IUserService
+      );
 
     @inject(TYPES.IAuthenticator)
     private readonly _authenticator: IAuthenticator;
 
-    public readonly users = async (_: void, args: void): Promise<IUser> => {
+    public readonly users = async (_: void, args: void): Promise<IUser[]> => {
         const users: IUser[] = await this._userService.getUsers();
-        return users[0]
+        return users
     }
 }
 
-// const users = GetUserController.users;
-// export const users = (_: void, args: void): IUser => {
-//   return {    
-//     _id: "1",
-//     name: "magda",
-//     email: "magda@gmail.com",
-//     password: "pass",
-//     role: UserRole.ADMIN
-//   }
-// }
--- backendGraphQl/src/schema/resolverMap.ts
+++ backendGraphQl/src/schema/resolverMap.ts
@@ -1,8 +1,8 @@
 import { IResolvers } from 'graphql-tools';
-import {GetUserController} from "../api/user/schema/resolver"
+import { UserResolver } from "../api/user/schema/resolver"
 const resolverMap: IResolvers = {
   Query: {
-    users: new GetUserController().users
+    users: new UserResolver().users
   },
 };
 export default resolverMap;
