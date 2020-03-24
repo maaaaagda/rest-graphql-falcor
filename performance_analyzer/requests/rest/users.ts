1ce1700e6fa3b942a55636b4eedfc16741131667
magdalena <magdapolak96@gmail.com>
2020-03-24 19:55:24 +0100

    fix: adjust users to got lib

--- performance_analyzer/requests/rest/users.ts
+++ performance_analyzer/requests/rest/users.ts
@@ -4,34 +4,35 @@ import got from "../got";
 import { generateRandomUser, generateTestUser } from "../../generate_data/users";
 import { MetricsResponse } from "../../types/Response";
 
-const options: any = {
-  url: API_URL + "users",
-  headers: {
-    "Access-Control-Allow-Origin": "*"
-  }
-};
-
-export const addUsers = async (nrOfUsers = 10, nrOfAdmins = 1, nrOfDietitians = 1): Promise<MetricsResponse> => {
+export const addUsers = async ({ nrOfUsers = 10, nrOfAdmins = 1, nrOfDietitians = 1, insertTestUser = false} = {})
+: Promise<MetricsResponse> => {
     let i: number = 0;
     let metrics = initialMetricsResponse;
-    options.method = "POST";
-    options.data = generateTestUser();
-    metrics = recalculateMetrics(metrics, await got(options));
+    const options = {
+        url: API_URL + "users",
+        method: "POST",
+        body: ""
+    };
+
+    if (insertTestUser) {
+        options.body = JSON.stringify(generateTestUser());
+        metrics = recalculateMetrics(metrics, await got(options));
+    }
 
     while (i < nrOfUsers) {
-        options.data = generateRandomUser();
+        options.body = JSON.stringify(generateRandomUser());
         metrics = recalculateMetrics(metrics, await got(options));
         i = i + 1;
     }
     i = 0;
     while (i < nrOfAdmins - 1 ) {
-        options.data = generateRandomUser("admin");
+        options.body = JSON.stringify(generateRandomUser("admin"));
         metrics = recalculateMetrics(metrics, await got(options));
         i = i + 1;
     }
     i = 0;
     while (i < nrOfDietitians) {
-        options.data = generateRandomUser("dietitian");
+        options.body = JSON.stringify(generateRandomUser("dietitian"));
         metrics = recalculateMetrics(metrics, await got(options));
         i = i + 1;
     }
@@ -39,9 +40,12 @@ export const addUsers = async (nrOfUsers = 10, nrOfAdmins = 1, nrOfDietitians = 
 };
 
 export const getAllUsers = async (token: string): Promise<MetricsResponse> => {
-    options.headers = {
+    const options = {
+        url: API_URL + "users",
+        headers: {
         ...got.defaults.options.headers,
         authorization: `Bearer ${token}`
+        }
     };
     const res = await got(options);
     return {
