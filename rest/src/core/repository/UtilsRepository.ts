b7044f08af30e917cbd48ba5006be7710bc154e3
magdalena <magdapolak96@gmail.com>
2020-04-17 21:58:20 +0200

    actually remove db collections from db

--- rest/src/core/repository/UtilsRepository.ts
+++ rest/src/core/repository/UtilsRepository.ts
@@ -1,8 +1,10 @@
 import { IUtilsRepository } from "./IUtilsRepository";
 import { inject, injectable } from "inversify";
-import { Connection } from "mongoose";
+import { Connection, Collection } from "mongoose";
 import { TYPES } from "../../ioc/types";
 import { IDatabase } from "../database/IDatabase";
+
+const collectionsToExcludeFromRemoving: string[] = ["products"];
 
 @injectable()
 export class UtilsRepository implements IUtilsRepository {
@@ -14,9 +16,13 @@ export class UtilsRepository implements IUtilsRepository {
   public async clearDatabase(): Promise<void> {
     const dbInstance: IDatabase = await this._db();
     const connection: Connection = await dbInstance.getConnection();
-    (await connection.db.listCollections().toArray()).forEach((collection: string) => {
-        console.log(collection);
-        // connection.db[collection].drop();
-    });
+    const collectionNames: string[] = (await connection.db.listCollections()
+      .toArray())
+      .map((collection: Collection) =>  collection.name);
+    for ( const collectionName of collectionNames) {
+      if (!collectionsToExcludeFromRemoving.includes(collectionName)) {
+        await connection.db.dropCollection(collectionName);
+      }
+    }
     }
 }
